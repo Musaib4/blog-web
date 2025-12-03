1e1d48166d6460894f427f608354ebe9c53e641f
Musaib Misger <musaibmisger0@gmail.com>
2025-12-03 21:29:31 +0530

    fix

--- script.js
+++ script.js
@@ -228,3 +228,24 @@ hamburger.addEventListener("click", () => {
 })();
 
 
+const ratio = 0.2;
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: ratio
+}
+
+function handleIntersect(entries, observer) {
+  entries.forEach(function(entry) {
+    if(entry.intersectionRatio > ratio) {
+      entry.target.classList.add('fx-reveal-visible')
+      observer.unobserve(entry.target);
+    }
+  })
+}
+
+const observer = new IntersectionObserver(handleIntersect, options);
+
+document.querySelectorAll('.fx-reveal').forEach(function(r) {
+  observer.observe(r);
+});
